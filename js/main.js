402593e688a1640f541321e157ea9d7cdff58f2f
Serxio <videmann@yandex.ru>
2019-10-12 13:50:53 +0300

    section--social

--- js/main.js
+++ js/main.js
@@ -12450,55 +12450,55 @@ jQuery(document).ready(function (jQuery) {
     }
     jQuery(".service__choice .service__tab").eq(i).css('display', 'block');
   });
-  // jQuery(function () {
-  //   ymaps.ready(init);
-  //   var myMap;
-  //   var coords = [56.853808, 60.644070];
-  //   function init() {
-  //     myMap = new ymaps.Map("mapDealer", {
-  //       center: coords,
-  //       zoom: 14,
-  //       controls: []
-  //     });
-  //     placeMark = new ymaps.Placemark(coords, {
-  //       //balloonContent: 'Адрес аптеки',
-  //     }, {
-  //       // Опции.
-  //       // Необходимо указать данный тип макета.
-  //       iconLayout: 'default#imageWithContent',
-  //       // Своё изображение иконки метки.
-  //       iconImageHref: 'img/icons/checkmark.svg',
-  //       // Размеры метки.
-  //       iconImageSize: [25, 25],
-  //       // Смещение левого верхнего угла иконки относительно
-  //       // её "ножки" (точки привязки).
-  //       iconImageOffset: [-24, -24],
-  //       // Смещение слоя с содержимым относительно слоя с картинкой.
-  //       iconContentOffset: [15, 15],
-  //     });
-  //     myMap.geoObjects.add(placeMark);
-  //     placeMark.events.add('click', function (e) {
-  //       var placemark = new ymaps.Placemark(coords, {
-  //         // Зададим содержимое заголовка балуна.
-  //         balloonContentHeader: '<div class="map__city">Екатеринбург</div>',
-  //         // Зададим содержимое основной части балуна.
-  //         balloonContentBody: '<div class="map__text">ООО "Соло"</div><div class="map__text">344000, Буденновский пр., дом №61/12</div><div class="map__text">Тел: (863) 263-71-10</div><div class="map__text">Тел: (863) 555-45-34</div><div class="map__text">E-mail: soloros@rambler.ru</div>',
-  //         // Зададим содержимое нижней части балуна.
-  //         //balloonContentFooter: '<button class="adress__button">Выбрать</button>',
-  //         // Зададим содержимое всплывающей подсказки.
-  //         hintContent: ''
-  //       });
-  //       // Добавим метку на карту.
-  //       myMap.geoObjects.add(placemark);
-  //       // Откроем балун на метке.
-  //       placemark.balloon.open();
-  //       placemark.events.add('balloonclose', function (e) {
-  //         myMap.geoObjects.remove(placemark);
-  //         placemark = null;
-  //       });
-  //     })
-  //   }
-  // });
+  jQuery(function () {
+    ymaps.ready(init);
+    var myMap;
+    var coords = [56.853808, 60.644070];
+    function init() {
+      myMap = new ymaps.Map("mapDealer", {
+        center: coords,
+        zoom: 14,
+        controls: []
+      });
+      placeMark = new ymaps.Placemark(coords, {
+        //balloonContent: 'Адрес аптеки',
+      }, {
+        // Опции.
+        // Необходимо указать данный тип макета.
+        iconLayout: 'default#imageWithContent',
+        // Своё изображение иконки метки.
+        iconImageHref: 'img/icons/checkmark.svg',
+        // Размеры метки.
+        iconImageSize: [25, 25],
+        // Смещение левого верхнего угла иконки относительно
+        // её "ножки" (точки привязки).
+        iconImageOffset: [-24, -24],
+        // Смещение слоя с содержимым относительно слоя с картинкой.
+        iconContentOffset: [15, 15],
+      });
+      myMap.geoObjects.add(placeMark);
+      placeMark.events.add('click', function (e) {
+        var placemark = new ymaps.Placemark(coords, {
+          // Зададим содержимое заголовка балуна.
+          balloonContentHeader: '<div class="map__city">Екатеринбург</div>',
+          // Зададим содержимое основной части балуна.
+          balloonContentBody: '<div class="map__text">ООО "Соло"</div><div class="map__text">344000, Буденновский пр., дом №61/12</div><div class="map__text">Тел: (863) 263-71-10</div><div class="map__text">Тел: (863) 555-45-34</div><div class="map__text">E-mail: soloros@rambler.ru</div>',
+          // Зададим содержимое нижней части балуна.
+          //balloonContentFooter: '<button class="adress__button">Выбрать</button>',
+          // Зададим содержимое всплывающей подсказки.
+          hintContent: ''
+        });
+        // Добавим метку на карту.
+        myMap.geoObjects.add(placemark);
+        // Откроем балун на метке.
+        placemark.balloon.open();
+        placemark.events.add('balloonclose', function (e) {
+          myMap.geoObjects.remove(placemark);
+          placemark = null;
+        });
+      })
+    }
+  });
   function mapsInit(coordinates, node) {
     jQuery(function () {
       ymaps.ready(init);
